e99162d5a39f002420c2d6f8da5d071e521fee91
Abduolimov03 <asadbekabduolimov33@gmail.com>
2025-10-27 10:00:12 +0500

    update

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
-from .models import Warehouse, Product, Employee, EmployeeInventory
+from .models import Warehouse, Product, Employee, EmployeeInventory, ProductLog
 
 
-admin.site.register(Warehouse)
-admin.site.register(Product)
-admin.site.register(Employee)
-admin.site.register(EmployeeInventory)
+@admin.register(Warehouse)
+class WarehouseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_central', 'address')
+    list_filter = ('is_central',)
+    search_fields = ('name', 'address')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'price', 'quantity', 'warehouse')
+    search_fields = ('name', 'code')
+    list_filter = ('warehouse',)
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'warehouse')
+
+
+@admin.register(EmployeeInventory)
+class EmployeeInventoryAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'product', 'date_added')
+
+
+@admin.register(ProductLog)
+class ProductLogAdmin(admin.ModelAdmin):
+    list_display = ('product', 'action', 'quantity', 'timestamp')
+    list_filter = ('action',)
